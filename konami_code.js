d357c5e87ef640e500bb704cd19dd9c66bf26516
Alsu Chalikova <alsu.halikova@gmail.com>
2019-03-20 19:53:57 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -16,10 +16,10 @@ let index = 0;
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
  console.log (key)
-  if (key === alphabet[index]) {
+  if (key === code[index]) {
     index++;
  
-    if (index === alphabet.length) {
+    if (index === code.length) {
       alert("Hurray!");
  
       index = 0;
